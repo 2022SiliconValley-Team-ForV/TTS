bed5326b5ea13c6d8b99b9a152b0585d3b85a501
baejunil <bjo6300@naver.com>
2022-07-18 13:25:14 +0900

    docs : ModelLink 기본키 명칭 변경

--- backend/backend/member/admin.py
+++ backend/backend/member/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Member, ModelLink
+
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ('id','name', 'birth', 'tmi', 'image_link')
+    search_fields =  ('name',)
+
+class ModelLinkAdmin(admin.ModelAdmin):
+    list_display = '__all__'
+
+admin.site.register(Member, MemberAdmin)
+admin.site.register(ModelLink, ModelLinkAdmin)
