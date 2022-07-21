6fdab0e6a9e9cf05285966facf927b0e6020456c
baejunil <bjo6300@naver.com>
2022-07-21 21:11:07 +0900

    feat : drf swagger 초기세팅

--- backend/mainApp/urls.py
+++ backend/mainApp/urls.py
@@ -1,13 +1,39 @@
 from django.urls import include, path
 from .views import *
 from rest_framework import routers
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 
 router = routers.DefaultRouter()
 router.register(r'members', MemberViewSet)
 router.register(r'modellinks', ModelLinkViewSet)
 router.register(r'texts', TextViewSet)
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title='TTS',
+        default_version='v1', # API 버전
+        description=
+        '''
+        API 문서 설명
+
+        ''',
+        terms_of_service='',
+        contact=openapi.Contact(name='이름', email='이메일'),
+        license=openapi.License(name='API 문서 이름')
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('', include(router.urls)),
+]
+
+# API 작성에 필요한 url 경로
+urlpatterns += [
+    path('swagger<str:format>', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('docs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
