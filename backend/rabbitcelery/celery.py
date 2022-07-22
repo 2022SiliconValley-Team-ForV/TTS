99ef48c44e2765dc85a7548eff51eda427dc0cae
suhyeon <suhyeon@naver.com>
2022-07-22 15:56:08 +0900

    feat: celery 연결, rabbitmq 환경변수 설정

--- backend/rabbitcelery/celery.py
+++ backend/rabbitcelery/celery.py
@@ -4,7 +4,9 @@ from celery import Celery
 app = Celery('rabbitcelery',
              # broker: //userid:password@hostname:port/virtual_host
              # RabbitMQ의 URL은 amqp://localhost
-             broker='amqp://tts:tts123@localhost/tts_host',
+             # local : hostname=localhost
+             # 도커 개발환경 : hostname=rabbit
+             broker='amqp://tts:tts123@rabbit/tts_host',
              # rpc는 결과를 AMQP 메시지로 보내는 것을 의미
              # task를 실행하고 난 뒤 결과물을 파일로 저장가능 // 다른 옵션
              backend='rpc://',
