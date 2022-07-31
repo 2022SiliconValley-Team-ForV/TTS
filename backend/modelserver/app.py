91052359a8b739b2dce117b037d0e9b6b5bdaaaa
hi-june <wnsgur735@gmail.com>
2022-07-31 23:55:50 +0900

    feat: celery tts test

--- backend/modelserver/app.py
+++ backend/modelserver/app.py
@@ -1,10 +1,7 @@
-from celery import Celery
 from flask import Flask, jsonify, request
-
 from flask_cors import CORS
-from simple_task import add
-import time
 
+from test_tasks import test
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
@@ -23,36 +20,14 @@ def hello_world():
 def get_text():
     if request.method == 'POST':
         params = request.get_json()
-        print(params)
         id = params['id']
-        print(id)
         text = params['text']
-        print(text)
-        a = add.delay(id, text)
-        # print(a.id)
-        return (params)
+        a = test.delay(id, text)
+        return {'task_id': a.id, 'task_status': a.ready()}
         
     elif request.method == 'GET':
-        a = add.delay(10, 20)
-        time.sleep(1)
-        result = a.get()
-        return {'task_id': a.id, 'task_status': a.ready(), 'task_result': result}
+        a = test.delay(5, '안녕하세요.')
+        return {'task_id': a.id, 'task_status': a.ready()}
         
 
 # front에서 셀러리가 일을 다 했는지 확인 -> 주기적으로 flask에 요청을 보내면서 체크 (작업이 걸리는 시간을 고려해서 작업끝나기 30초 전부터 보낸다)
-
-# @app.route('/mecab')
-# def get_mecab():
-#     k_str = request.args.get('q', u'영등포구청역에 있는 맛집 좀 알려주세요.')
-#     morphs = mecab.morphs(k_str)
-#     nouns = mecab.nouns(k_str)
-#     pos = mecab.pos(k_str)
-#     return {'morphs': morphs, 'nouns': nouns, 'pos': pos}
-
-
-# @app.route('/g2pk')
-# def run_g2pk():
-#     k_str = request.args.get('q', u'신을 신고 얼른 동사무소에 가서 혼인 신고 해라')
-#     print(k_str)
-#     res = gp(k_str)
-#     return {'result': res}
--- backend/modelserver/celery_app.py
+++ backend/modelserver/celery_app.py
@@ -1,8 +1,7 @@
-from xml.etree.ElementInclude import include
 from celery import Celery
 
 app = Celery('celery',
              broker='amqp://tts:tts123@rabbit/tts_host',
              backend='rpc://',
-             include=['simple_task'],
+             include=['test_tasks'],
              )
