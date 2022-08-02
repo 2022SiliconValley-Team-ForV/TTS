e5ddc67f5a77b3b6f10e54c20a9d027486a685f3
hi-june <wnsgur735@gmail.com>
2022-08-02 21:56:58 +0900

    refactor: gcs object name changed

--- backend/modelserver/test_tasks.py
+++ backend/modelserver/test_tasks.py
@@ -36,8 +36,8 @@ bucket = storage_client.bucket(bucket_name)
 ############################################################################################################
 @app.task(name="test")
 def test(uuid, member_id, text, create_at):
-    wav_file = f'{member_id}_{uuid}_{create_at}_voice.wav'
-    wav_path = f'./temp/{wav_file}'
+    wav_file = f'wav_files/{member_id}/{uuid}_{create_at}_voice.wav'
+    wav_path = f'./temp/{member_id}_{uuid}_{create_at}_voice.wav'
     n_text = normalize_text(text, symbol)
     
     wav = synthesizer.tts(n_text, None, None)
