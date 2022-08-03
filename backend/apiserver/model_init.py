abb37eb4a0a60d45e37dbb304da852cd7959c69c
hi-june <wnsgur735@gmail.com>
2022-08-04 01:47:16 +0900

    feat: model init dockerfile changed

--- backend/apiserver/model_init.py
+++ backend/apiserver/model_init.py
@@ -14,7 +14,7 @@ def filedownload(url1, url2, url3, path):
     wget.download(url3, path)
 
 
-if os.path.isfile("../modelserver/voice_model/glow-tts/1/1g_config.json"):
+if os.path.isfile("modelserver/voice_model/glow-tts/1/1g_config.json"):
     print("이미 존재합니다!")
     pass
 
@@ -22,14 +22,17 @@ else:
     for i in range(1,6):
 
         i = str(i)
-        save_path = f'../modelserver/voice_model/glow-tts/{i}'  # 다운받을 파일을 저장할 경로
+        save_path = f'modelserver/voice_model/glow-tts/{i}'  # 다운받을 파일을 저장할 경로
         modellink = ModelLink.objects.get(id=i)
         print(modellink.glow_config)
         
+        print('downloading start!!')
         filedownload(modellink.glow_pth, modellink.glow_config, modellink.glow_scale_stats, save_path)
+        print('downloading end!!')
+
 
         # hifi 경로
-        save_path = f'../modelserver/voice_model/hifigan-v2/{i}'    # 다운받을 파일을 저장할 경로
+        save_path = f'modelserver/voice_model/hifigan-v2/{i}'    # 다운받을 파일을 저장할 경로
 
         filedownload(modellink.hifi_pth, modellink.hifi_config, modellink.hifi_scale_stats, save_path)
 
