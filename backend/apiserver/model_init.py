13e21e66d247afee917147c24ad875e29d5614eb
baejunil <bjo6300@naver.com>
2022-08-03 22:09:55 +0900

    fix : fix model_init path

--- backend/apiserver/model_init.py
+++ backend/apiserver/model_init.py
@@ -13,18 +13,25 @@ def filedownload(url1, url2, url3, path):
     wget.download(url2, path)
     wget.download(url3, path)
 
-for i in range(1,6):
 
-    i = str(i)
-    save_path = '../modelserver/voice_model/glow-tts/' + i   # 다운받을 파일을 저장할 경로
-    modellink = ModelLink.objects.get(id=i)
-    
-    filedownload(modellink.glow_pth, modellink.glow_config, modellink.glow_scale_stats, save_path)
+if os.path.isfile("../modelserver/voice_model/glow-tts/1/1g_config.json"):
+    print("이미 존재합니다!")
+    pass
 
-    # hifi 경로
-    save_path = '../modelserver/voice_model/hifigan-v2/' + i    # 다운받을 파일을 저장할 경로
+else:
+    for i in range(1,6):
 
-    filedownload(modellink.hifi_pth, modellink.hifi_config, modellink.hifi_scale_stats, save_path)
+        i = str(i)
+        save_path = f'../modelserver/voice_model/glow-tts/{i}'  # 다운받을 파일을 저장할 경로
+        modellink = ModelLink.objects.get(id=i)
+        print(modellink.glow_config)
+        
+        filedownload(modellink.glow_pth, modellink.glow_config, modellink.glow_scale_stats, save_path)
+
+        # hifi 경로
+        save_path = f'../modelserver/voice_model/hifigan-v2/{i}'    # 다운받을 파일을 저장할 경로
+
+        filedownload(modellink.hifi_pth, modellink.hifi_config, modellink.hifi_scale_stats, save_path)
 
     
     
