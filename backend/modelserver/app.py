e44173671fa251de553eeba559230400e64f25fa
hi-june <wnsgur735@gmail.com>
2022-08-03 13:39:51 +0900

    feat: celery containter seperate success!
    docker compose 파일의 depends on을 수정했더니 컨테이너 분리에 성공했습니다.

--- backend/modelserver/app.py
+++ backend/modelserver/app.py
@@ -3,7 +3,6 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 #from test_tasks import test
-#from simple_task import add
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
